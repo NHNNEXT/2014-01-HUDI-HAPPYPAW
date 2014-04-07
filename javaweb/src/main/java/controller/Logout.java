687d728f90dc05468be9082b9882c69d5047edea
dayoungles <dayoungles@naver.com>
2014-04-07 14:10:48 +0900

    add nyamhistory

--- javaweb/src/main/java/controller/Logout.java
+++ javaweb/src/main/java/controller/Logout.java
@@ -16,18 +16,12 @@ public class Logout {
 			throws ServletException, IOException {
 		DAO db = DAO.getInstance();
 		
-		
-		HttpSession session = request.getSession();
-		String users_id = (String) session.getAttribute("users_id");
+		HttpSession session = request.getSession(false);
+		if(session != null){
+			session.invalidate();
+		}
 		if(users_id == null || users_id.equals("")){
-			//로그인 과정 
-			String jspId = request.getParameter("id");//jsp에서 보낸 id
-			String jspPs = request.getParameter("password");
-			User user = db.getUser(jspId);
-			if(user.checkPs(jspPs)){
-				session.setAttribute("users_id", jspId);
-				response.sendRedirect("./nyamHistory");
-				System.out.println("login complete :"+ jspId);
+			
 			} else {
 				response.sendRedirect("./login");
 			}
---
+++ javaweb/src/main/java/controller/NyamHistory.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DAO;
+import model.StampHistory;
+
+public class NyamHistory extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DAO db = DAO.getInstance();
+		ArrayList<StampHistory> stampList=db.selectMonthHistory("121001");
+		request.setAttribute("record", stampList);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("nyamHistory.jsp");
+		dispatcher.forward(request, response);
+
+	}
+}
